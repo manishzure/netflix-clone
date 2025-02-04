b1c028f17b18ac26484c341fc8f552f5a764beb4
manishzure <manishzure31@gmail.com>
2025-02-04 16:25:52 +0530

    frontend left

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken';
+import { ENV_VARS } from "../config/envVars";
+import { User } from '../models/user.model';
+
+
---
+++ backend/routes/movie.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getTrendingMovie, getMovieTrailers, getMovieDetails, getSimilarMovie, getMovieByCategory } from "../controllers/movie.controller.js";
+
+const router = express.Router();
+
+router.get("/trending", getTrendingMovie); 
+router.get("/:id/trailers", getMovieTrailers); 
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovie);
+router.get("/:category", getMovieByCategory); 
+
+
+
+
+
+export default router;
